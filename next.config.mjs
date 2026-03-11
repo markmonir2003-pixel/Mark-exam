f2e5661425defa4a9693b4e12142d238d064d5c6
Mirk <mirk@example.com>
2026-03-11 23:34:49 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // تعطيل فحص TypeScript أثناء مرحلة البناء
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
+  // تعطيل فحص ESLint أثناء مرحلة البناء
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-}
+  // إذا كنت تستخدم التصدير الثابت SPA
+  // output: 'export',
+  // ...
+};
 
-export default nextConfig
+export default nextConfig;
